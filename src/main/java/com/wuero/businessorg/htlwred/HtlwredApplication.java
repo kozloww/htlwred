9fa5be947880b6c2751a0ceff341d6213320b475
kozloww <maximilian.ho@yahoo.com>
2021-08-03 13:20:35 +0200

    Initial commit

---
+++ src/main/java/com/wuero/businessorg/htlwred/HtlwredApplication.java
@@ -0,0 +1,13 @@
+package com.wuero.businessorg.htlwred;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtlwredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HtlwredApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wuero/businessorg/htlwred/HtlwredApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wuero.businessorg.htlwred;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtlwredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
