872d52761e0218092002790cb2eb35aae9d662d7
kozloww <maximilian.ho@yahoo.com>
2021-08-03 15:43:53 +0200

    1

--- src/main/java/com/wuero/businessorg/htlwred/HtlwredApplication.java
+++ src/main/java/com/wuero/businessorg/htlwred/HtlwredApplication.java
@@ -2,12 +2,20 @@ package com.wuero.businessorg.htlwred;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
 public class HtlwredApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(HtlwredApplication.class, args);
+	}
+	@GetMapping
+	String greet(@RequestParam("name")String name) {
+		return "hello " + name;
 	}
 
 }
+
