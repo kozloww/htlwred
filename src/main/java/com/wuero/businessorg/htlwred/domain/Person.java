6dec58966986802b2041ec561abe8fc8baa274ef
kozloww <maximilian.ho@yahoo.com>
2021-08-04 14:08:41 +0200

    überarbeitet4

---
+++ src/main/java/com/wuero/businessorg/htlwred/domain/Person.java
@@ -0,0 +1,25 @@
+package com.wuero.businessorg.htlwred.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Person {
+
+    @Id
+    private int id;
+
+    private String name;
+
+    private String lastname;
+
+    private LocalDate birthDate;
+
+
+}
--- src/main/resources/schema-h2.sql
+++ src/main/resources/schema-h2.sql
@@ -1,8 +1,8 @@
-drop table if exsits person;
+drop table if exists person;
 
 create table person
 (
-   id integer not null auto increment,
+   id integer not null primary key,
    name varchar,
    last_name varchar,
    birthdate date
